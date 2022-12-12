dcde2b72b7c419f6e35976dc3ad58b7a53b27e24
Sylv11 <sylvain.urbain.isf@gmail.com>
2022-12-12 22:51:56 +0100

    [IMP] 27 Shaders WIP Uniforms

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -23,9 +23,7 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
-/**
- * Test mesh
- */
+
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
@@ -42,10 +40,19 @@ geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
 const material = new THREE.RawShaderMaterial({
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
+    uniforms: {
+        uFrequency: { value: new THREE.Vector2(10.0, 5) },
+        uTime: { value: 0 },
+        uColor: { value: new THREE.Color('orange') },
+    }
 })
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2 / 3
 scene.add(mesh)
 
 /**
@@ -98,6 +105,9 @@ const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update material
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
