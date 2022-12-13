33d801b42c8dcb840a55ed59f43fd1b681408059
Sylv11 <sylvain.urbain.isf@gmail.com>
2022-12-13 22:06:22 +0100

    [IMP] 27 Shaders END

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -22,6 +22,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const flagTexture = textureLoader.load('/textures/flag-french.jpg')
 
 
 // Geometry
@@ -37,13 +38,14 @@ for (let i = 0; i < count; i++) {
 geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
 
 // Material
-const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
     uniforms: {
         uFrequency: { value: new THREE.Vector2(10.0, 5) },
         uTime: { value: 0 },
         uColor: { value: new THREE.Color('orange') },
+        uTexture: { value: flagTexture }
     }
 })
 
